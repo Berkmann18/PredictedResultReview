ec75e8b1cf252d9c8e3e1b76fb903d8ac0f72bd5
Berkmann18 <maxieberkmann@gmail.com>
2018-11-07 15:12:55 +0000

    New(Front-End): Added a VueJS app
    Added a draft of the client app
    
    re #4

---
+++ client/src/components/Panel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    fill-height
+  >
+    <v-toolbar
+      color="cyan"
+      class="white--text"
+    >
+      <v-toolbar-title>
+        {{title}}
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>refresh</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <!-- Perhaps have a line-numbered v-textarea -->
+    <v-card>
+      <v-container fluid>
+        <v-textarea>
+        </v-textarea>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Panel',
+  props: {
+    title: String
+  }
+}
+</script>
---
+++ client/src/components/PanelDiff.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    fill-height
+  >
+    <v-toolbar
+      color="cyan"
+      class="white--text"
+    >
+      <v-toolbar-title>
+        {{title}}
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>refresh</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <!-- Perhaps have it line-numbered -->
+    <v-card>
+      <v-container fluid>
+        <div id="diff"></div>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PanelDiff',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .plus { //green accent-3
+    background-color: #80ff80;
+    color: #0f0;
+  }
+
+  .minus { //red accent-3
+    background-color: #ff8080;
+    color: #f00;
+  }
+</style>
