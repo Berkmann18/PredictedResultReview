cb16d4a179983fc017d506186fb725b36646cb36
Berkmann18 <maxieberkmann@gmail.com>
2018-11-07 15:34:13 +0000

    Update(Front-End): Added line numbers
    re #4

--- client/src/components/Panel.vue
+++ client/src/components/Panel.vue
@@ -7,18 +7,24 @@
       class="white--text"
     >
       <v-toolbar-title>
-        {{title}}
+        {{ title }}
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>refresh</v-icon>
       </v-btn>
     </v-toolbar>
-    <!-- Perhaps have a line-numbered v-textarea -->
     <v-card>
       <v-container fluid>
-        <v-textarea>
-        </v-textarea>
+        <!-- @todo Check if this renders properly, maybe check out https://github.com/Shenmin-Z/lined-textarea -->
+        <span
+          v-for="line in lineCount"
+          :key="line"
+          >
+        </span>
+        <v-textarea
+          v-model="text"
+        ></v-textarea>
       </v-container>
     </v-card>
   </div>
@@ -29,6 +35,16 @@ export default {
   name: 'Panel',
   props: {
     title: String
+  },
+  data () {
+    return {
+      text: ''
+    }
+  },
+  computed: {
+    lineCount: () => {
+      return this.text.length ? this.text.split(/\r\n|\r|\n/).length : 0;
+    }
   }
 }
 </script>
