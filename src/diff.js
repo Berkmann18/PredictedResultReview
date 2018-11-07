310a5ff35399f28f7bf7d3f502fdec29ea08945e
Berkmann18 <maxieberkmann@gmail.com>
2018-11-07 15:12:55 +0000

    New(Front-End): Added a VueJS app
    Added a draft of the client app
    
    re #4

--- src/diff.js
+++ src/diff.js
@@ -9,6 +9,8 @@
 const fs = require('fs')
 const { error } = require('nclr')
 
+const { error } = require('nclr')
+
 /**
  * @description Gets the matrix equivalent of a string.
  * @param {string} data Data to to convert
