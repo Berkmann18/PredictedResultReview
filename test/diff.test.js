1146fc601c6cf8378eeaede0595218b61e392a92
Berkmann18 <maxieberkmann@gmail.com>
2018-11-27 17:08:50 +0000

    Update(Vue+Source): Refactoring
    Refactored `fileToArr` into having the line treatment inside `strToMtx` and added tests and ESM
    version for the client.

--- test/diff.test.js
+++ test/diff.test.js
@@ -1,76 +1,78 @@
-const { difference, diff, fileToArr } = require('../src/diff');
+const { difference, diff, fileToArr, strToMtx } = require('../src/diff')
+
+/* global test:false, expect:false */
 
 test('difference', () => {
-  let a = [0, 1, 2, 4], b = [0, 1, 2, 3];
+  let a = [0, 1, 2, 4]; let b = [0, 1, 2, 3]
   expect(difference(a, b)).toStrictEqual([{
     idx: 3,
     before: 4,
     after: 3
-  }]);
+  }])
 
-  a = [0, 1.5, 2, 3];
-  b = [0, 1.2, 2, 3];
+  a = [0, 1.5, 2, 3]
+  b = [0, 1.2, 2, 3]
   expect(difference(a, b)).toStrictEqual([{
     idx: 1,
     before: 1.5,
     after: 1.2
-  }]);
+  }])
 
-  a = ['0', 1, 2];
-  b = [0, 1, 2];
+  a = ['0', 1, 2]
+  b = [0, 1, 2]
   expect(difference(a, b)).toStrictEqual([{
     idx: 0,
     before: '0',
     after: 0
-  }]);
+  }])
 
-  expect(difference(b, b)).toStrictEqual([]);
-});
+  expect(difference(b, b)).toStrictEqual([])
+})
 
 test('diff error', () => {
-  let a = [0, 1, 2, 4], b = [0, 1, 2, 3];
-  expect(() => diff(a, b)).toThrowError('Empty array(s)');
-});
+  let a = [0, 1, 2, 4]; let b = [0, 1, 2, 3]
+  expect(() => diff(a, b)).toThrowError('Empty array(s)')
+})
 
 test('diff', () => {
-  let a = [0, 1, 2, 4], b = [0, 1, 2, 3];
+  let a = [0, 1, 2, 4]; let b = [0, 1, 2, 3]
   expect(diff([a], [b])).toStrictEqual([[{
     idx: 3,
     before: 4,
     after: 3
-  }]]);
-});
+  }]])
+})
 
 test('diff', () => {
-  let a = [0, 1, 2, 4], b = [0, 1, 2, 3];
-  expect(() => diff([a], [b, []])).toThrowError('Either the matrices are empty or of a different size');
-});
+  let a = [0, 1, 2, 4]; let b = [0, 1, 2, 3]
+  expect(() => diff([a], [b, []])).toThrowError('Either the matrices are empty or of a different size')
+})
 
 test('file->matrix', () => {
-  expect.assertions(1);
-  let data = [['Person', 'Rank'], ['John', '1'], ['Mark', '2'], ['Jack', '3']];
+  expect.assertions(1)
+  let data = [['Person', 'Rank'], ['John', '1'], ['Mark', '2'], ['Jack', '3']]
   return fileToArr('./examples/before.txt')
     .then(
       res => expect(res).toStrictEqual(data),
       err => expect(() => err).toThrowError(Error)
     )
-    .catch(err => console.log('Error=', err));
-});
+    .catch(err => console.log('Error=', err))
+})
 
-//L.23 (FS error)
+// L.23 (FS error)
 
 test('before vs after', () => {
-  expect.assertions(1);
+  expect.assertions(1)
   let delta = [[], [], [{
-      idx: 1,
-      before: '2',
-      after: '3'
-    }], [{
-      idx: 1,
-      before: '3',
-      after: '2'
-    }]
-  ];
+    idx: 1,
+    before: '2',
+    after: '3'
+  }], [{
+    idx: 1,
+    before: '3',
+    after: '2'
+  }]
+  ]
 
   return fileToArr('./examples/before.txt')
     .then(before => {
@@ -79,4 +81,14 @@ test('before vs after', () => {
         .catch(err => console.log('after.txt failed\n', err))
     })
     .catch(err => console.log('before.txt failed\n', err))
-});
+})
+
+test('string->matrix', () => {
+  expect(strToMtx('Lorem Ipsum')).toStrictEqual([['Lorem Ipsum']])
+  expect(strToMtx('Lorem,Ipsum')).toStrictEqual([['Lorem', 'Ipsum']])
+  expect(strToMtx('Lorem Ipsum', ' ')).toStrictEqual([['Lorem', 'Ipsum']])
+  expect(strToMtx('Lorem Ipsum')).toStrictEqual([['Lorem Ipsum']])
+  expect(strToMtx('Lorem Ipsum\nDolore Sit')).toStrictEqual([['Lorem Ipsum'], ['Dolore Sit']])
+  expect(strToMtx('Lorem Ipsum\nDolore Sit', ' ')).toStrictEqual([['Lorem', 'Ipsum'], ['Dolore', 'Sit']])
+  expect(strToMtx('Lorem Ipsum;Dolore Sit', ' ', ';')).toStrictEqual([['Lorem', 'Ipsum'], ['Dolore', 'Sit']])
+})
